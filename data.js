acfd6f72a2a92d39425305069150f2f65ea66834
Olga <o.nikolaichenko@ya.ru>
2024-02-13 15:20:04 +0100

    product data added

---
+++ data.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const dataProducts = `[
+  {
+    "productId": "1",
+    "productImage": "images/2.png",
+    "productName": "MANGO PEOPLE T-SHIRT",
+    "productPrice": "300",
+    "productColor": "Red",
+    "productSize": "XL",
+    "productQuantity: "2"
+  },
+  {
+    "productId": "1",
+    "productImage": "images/3.png",
+    "productName": "MANGO PEOPLE T-SHIRT",
+    "productPrice": "300",
+    "productColor": "Red",
+    "productSize": "XL",
+    "productQuantity: "2"
+  }
+]`;
